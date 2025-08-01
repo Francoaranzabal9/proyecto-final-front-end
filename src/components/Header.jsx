56b0d27fad2152091b4d335ee91d62af28b1c14b
francoaranzabal9 <juanfrancoaranzabal@gmail.com>
2025-08-01 17:22:29 -0300

    add: incorporacion de useAuth y rutas privadas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,33 @@
 import { Link } from "react-router-dom"
-
+import { useAuth } from "../context/UserContext"
 
 
 
 const Header = () => {
+  const { user, logout } = useAuth()
+
+  const handleLogout = () => {
+    logout()
+  }
+
+
   return (
     <nav>
       {/* <img src="" alt="Logo de la tienda" /> */}
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/Dashboard">Dashboard</Link></li>
-        <li><Link to="/Login">Login</Link></li>
-        <li><Link to="/Register">Register</Link></li>
+        {
+          user && <div>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/Dashboard">Dashboard</Link></li>
+            <button onClick={handleLogout}>Cerrar sesion</button>
+          </div>
+        }
+        {
+          !user && <div>
+            <li><Link to="/Login">Login</Link></li>
+            <li><Link to="/Register">Register</Link></li>
+          </div>
+        }
       </ul>
     </nav>
   )
