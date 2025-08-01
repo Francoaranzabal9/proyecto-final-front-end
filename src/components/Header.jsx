a563add140258e457b68e6133e98710c50801f2f
francoaranzabal9 <juanfrancoaranzabal@gmail.com>
2025-08-01 15:56:19 -0300

    add: Incorporando logica en Login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom"
 const Header = () => {
   return (
     <nav>
-      <img src="" alt="Logo de la tienda" />
+      {/* <img src="" alt="Logo de la tienda" /> */}
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/Dashboard">Dashboard</Link></li>
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,24 +1,45 @@
 import { Layout } from "../components/Layout"
-
+import { useState } from "react"
 
 
 
 
 const Login = () => {
+
+  const [password, setPassword] = useState("")
+  const [username, setUsername] = useState("")
+
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+    console.log("usuario logeado")
+
+    setUsername("")
+    setPassword("")
+  }
+
+
+
   return (
     <Layout>
       <section>
         <h1>Inicia sesion</h1>
       </section>
       <section>
-        <form>
+        <form onSubmit={handleLogin}>
           <div>
             <label >Nombre de usuario:</label>
-            <input type="text" />
+            <input
+              type="text"
+              onChange={(e) => setUsername(e.target.value)}
+              value={username} />
           </div>
           <div>
             <label >Contraseña:</label>
-            <input type="password" />
+            <input
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password} />
           </div>
           <button>Ingresar</button>
         </form>
