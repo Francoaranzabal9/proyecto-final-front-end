94884cc6fc5ab20425763b85abe882c51d3fd5d0
francoaranzabal9 <juanfrancoaranzabal@gmail.com>
2025-08-01 15:08:12 -0300

    add: Pagina NotFound

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,12 +1,20 @@
+import { Link } from "react-router-dom"
+import { Layout } from "../components/Layout"
 
 
 
 
 
 const NotFound = () => {
-
-
-
+  return (
+    <Layout>
+      <div>
+        <h1>404</h1>
+        <p>La página que buscas no existe.</p>
+        <a><Link to="/">Volver al inicio</Link></a>
+      </div>
+    </Layout>
+  )
 }
 
 
