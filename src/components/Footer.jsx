3a204bc9aab820e39309b7b868c9b331ea70583d
francoaranzabal9 <juanfrancoaranzabal@gmail.com>
2025-08-05 23:44:45 -0300

    add: estilando footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,18 @@
-
-
-
+import "../styles/footer.css"
 
 
 const Footer = () => {
   return (
-    <p>Sitio creado por Juan Franco</p>
+    <section>
+      <div className="footer-cont">
+        <p>Sitio creado por <a href="https://www.linkedin.com/in/juan-franco-aranzabal-5a63762b7/" target="blank_">Juan Franco Aranzabal</a></p>
+        <div className="footer-links">
+          <a href="https://github.com/Francoaranzabal9"><img src="https://img.icons8.com/?size=100&id=3tC9EQumUAuq&format=png&color=FFFFFF" alt="icono de git" /></a>
+          <a href="https://steamcommunity.com/profiles/76561199249750087/"><img src="https://img.icons8.com/?size=100&id=kBZ9gLW27vrQ&format=png&color=FFFFFF" alt="icono de steam" /></a>
+          <a href="https://www.instagram.com/francoaranzabal9/"><img src="https://img.icons8.com/?size=100&id=RhYNENh5cxlS&format=png&color=FFFFFF" alt="icono de instagram" /></a>
+        </div>
+      </div>
+    </section>
   )
 }
 
