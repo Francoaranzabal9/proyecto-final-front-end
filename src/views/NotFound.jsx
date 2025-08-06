f55d57bbebc42b65a25bc9aef69207f1a67ef2e4
francoaranzabal9 <juanfrancoaranzabal@gmail.com>
2025-08-06 18:36:21 -0300

    add: estilando notFound

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,18 +1,24 @@
 import { Link } from "react-router-dom"
 import { Layout } from "../components/Layout"
 
+import notFoundImg from "../assets/not-found-img.png"
 
-
-
+import "../styles/notfound.css"
 
 const NotFound = () => {
   return (
     <Layout>
-      <div>
-        <h1>404</h1>
-        <p>La página que buscas no existe.</p>
-        <a><Link to="/">Volver al inicio</Link></a>
-      </div>
+      <section className="not-found-section">
+        <div className="not-found-cont">
+          <img src={notFoundImg} alt="Imagen 404" />
+          <div className="not-found-text">
+            <h1>404</h1>
+            <p><strong>No podemos encontrar la página que estás buscando.</strong></p>
+            <p>Puede que haya expirado, o podría haber un error de escritura. Tal vez puedas encontrar lo que necesitas desde nuestra página principal.</p>
+            <Link className="not-found-link" to="/">Volver al inicio</Link>
+          </div>
+        </div>
+      </section>
     </Layout>
   )
 }
